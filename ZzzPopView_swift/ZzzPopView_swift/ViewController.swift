2303ebd8403c573b58eb27f4920bec1bde73adfa
zhouzezhou <zhouze4zhou33@sina.com>
2018-08-30 22:21:59 +0800

    hide uncompleted function

--- ZzzPopView_swift/ZzzPopView_swift/ViewController.swift
+++ ZzzPopView_swift/ZzzPopView_swift/ViewController.swift
@@ -36,64 +36,64 @@ class ViewController: UIViewController {
             make.height.equalTo(heightBtn)
         }
         
-        let stripScrollSelectBtn = UIButton.init(type: .custom)
-        stripScrollSelectBtn.backgroundColor = .red
-        stripScrollSelectBtn.setTitle("纵向TableView的Popview", for: .normal)
-        stripScrollSelectBtn.addTarget(self, action: #selector(self.stripScrollSelectBtnClick) , for: .touchUpInside)
-        stripScrollSelectBtn.layer.cornerRadius = 4.0
-        self.view!.addSubview(stripScrollSelectBtn)
-        
-        stripScrollSelectBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(regularSelectBtn.snp.bottom).offset(heightBtn)
-            make.left.equalTo(self.view).offset(10)
-            make.width.equalTo(kScreenWidth - 20)
-            make.height.equalTo(heightBtn)
-        }
-        
-        
-        let zzz6NumberInputPopViewBtn = UIButton.init(type: .custom)
-        zzz6NumberInputPopViewBtn.backgroundColor = .red
-        zzz6NumberInputPopViewBtn.setTitle("6位数输入Popview", for: .normal)
-        zzz6NumberInputPopViewBtn.addTarget(self, action: #selector(self.zzz6NumberInputPopViewBtnClick) , for: .touchUpInside)
-        zzz6NumberInputPopViewBtn.layer.cornerRadius = 4.0
-        self.view!.addSubview(zzz6NumberInputPopViewBtn)
-
-        zzz6NumberInputPopViewBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(stripScrollSelectBtn.snp.bottom).offset(heightBtn)
-            make.left.equalTo(self.view).offset(10)
-            make.width.equalTo(kScreenWidth - 20)
-            make.height.equalTo(heightBtn)
-        }
-
-
-        let ZzzBankCardChoosePopViewBtn = UIButton.init(type: .custom)
-        ZzzBankCardChoosePopViewBtn.backgroundColor = .red
-        ZzzBankCardChoosePopViewBtn.setTitle("银行卡选择Popview", for: .normal)
-        ZzzBankCardChoosePopViewBtn.addTarget(self, action: #selector(self.ZzzBankCardChoosePopViewBtnClick) , for: .touchUpInside)
-        ZzzBankCardChoosePopViewBtn.layer.cornerRadius = 4.0
-        self.view!.addSubview(ZzzBankCardChoosePopViewBtn)
-
-        ZzzBankCardChoosePopViewBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(zzz6NumberInputPopViewBtn.snp.bottom).offset(heightBtn)
-            make.left.equalTo(self.view).offset(10)
-            make.width.equalTo(kScreenWidth - 20)
-            make.height.equalTo(heightBtn)
-        }
-
-
-        let ZzzDateSelectPopviewBtn = UIButton.init(type: .custom)
-        ZzzDateSelectPopviewBtn.backgroundColor = .red
-        ZzzDateSelectPopviewBtn.setTitle("起始和终止日期选择Popview", for: .normal)
-        ZzzDateSelectPopviewBtn.addTarget(self, action: #selector(self.ZzzDateSelectPopviewBtnClick) , for: .touchUpInside)
-        ZzzDateSelectPopviewBtn.layer.cornerRadius = 4.0
-        self.view!.addSubview(ZzzDateSelectPopviewBtn)
-
-        ZzzDateSelectPopviewBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(ZzzBankCardChoosePopViewBtn.snp.bottom).offset(heightBtn)
-            make.left.equalTo(self.view).offset(10)
-            make.width.equalTo(kScreenWidth - 20)
-            make.height.equalTo(heightBtn)
-        }
+//        let stripScrollSelectBtn = UIButton.init(type: .custom)
+//        stripScrollSelectBtn.backgroundColor = .red
+//        stripScrollSelectBtn.setTitle("纵向TableView的Popview", for: .normal)
+//        stripScrollSelectBtn.addTarget(self, action: #selector(self.stripScrollSelectBtnClick) , for: .touchUpInside)
+//        stripScrollSelectBtn.layer.cornerRadius = 4.0
+//        self.view!.addSubview(stripScrollSelectBtn)
+//        
+//        stripScrollSelectBtn.snp.makeConstraints { (make) in
+//            make.top.equalTo(regularSelectBtn.snp.bottom).offset(heightBtn)
+//            make.left.equalTo(self.view).offset(10)
+//            make.width.equalTo(kScreenWidth - 20)
+//            make.height.equalTo(heightBtn)
+//        }
+//        
+//        
+//        let zzz6NumberInputPopViewBtn = UIButton.init(type: .custom)
+//        zzz6NumberInputPopViewBtn.backgroundColor = .red
+//        zzz6NumberInputPopViewBtn.setTitle("6位数输入Popview", for: .normal)
+//        zzz6NumberInputPopViewBtn.addTarget(self, action: #selector(self.zzz6NumberInputPopViewBtnClick) , for: .touchUpInside)
+//        zzz6NumberInputPopViewBtn.layer.cornerRadius = 4.0
+//        self.view!.addSubview(zzz6NumberInputPopViewBtn)
+//
+//        zzz6NumberInputPopViewBtn.snp.makeConstraints { (make) in
+//            make.top.equalTo(stripScrollSelectBtn.snp.bottom).offset(heightBtn)
+//            make.left.equalTo(self.view).offset(10)
+//            make.width.equalTo(kScreenWidth - 20)
+//            make.height.equalTo(heightBtn)
+//        }
+//
+//
+//        let ZzzBankCardChoosePopViewBtn = UIButton.init(type: .custom)
+//        ZzzBankCardChoosePopViewBtn.backgroundColor = .red
+//        ZzzBankCardChoosePopViewBtn.setTitle("银行卡选择Popview", for: .normal)
+//        ZzzBankCardChoosePopViewBtn.addTarget(self, action: #selector(self.ZzzBankCardChoosePopViewBtnClick) , for: .touchUpInside)
+//        ZzzBankCardChoosePopViewBtn.layer.cornerRadius = 4.0
+//        self.view!.addSubview(ZzzBankCardChoosePopViewBtn)
+//
+//        ZzzBankCardChoosePopViewBtn.snp.makeConstraints { (make) in
+//            make.top.equalTo(zzz6NumberInputPopViewBtn.snp.bottom).offset(heightBtn)
+//            make.left.equalTo(self.view).offset(10)
+//            make.width.equalTo(kScreenWidth - 20)
+//            make.height.equalTo(heightBtn)
+//        }
+//
+//
+//        let ZzzDateSelectPopviewBtn = UIButton.init(type: .custom)
+//        ZzzDateSelectPopviewBtn.backgroundColor = .red
+//        ZzzDateSelectPopviewBtn.setTitle("起始和终止日期选择Popview", for: .normal)
+//        ZzzDateSelectPopviewBtn.addTarget(self, action: #selector(self.ZzzDateSelectPopviewBtnClick) , for: .touchUpInside)
+//        ZzzDateSelectPopviewBtn.layer.cornerRadius = 4.0
+//        self.view!.addSubview(ZzzDateSelectPopviewBtn)
+//
+//        ZzzDateSelectPopviewBtn.snp.makeConstraints { (make) in
+//            make.top.equalTo(ZzzBankCardChoosePopViewBtn.snp.bottom).offset(heightBtn)
+//            make.left.equalTo(self.view).offset(10)
+//            make.width.equalTo(kScreenWidth - 20)
+//            make.height.equalTo(heightBtn)
+//        }
     }
 
     //MARK: - Event
